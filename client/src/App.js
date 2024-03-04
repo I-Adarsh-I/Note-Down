5cb8a49e9f24373ef97ae26ee053bb7185a75091
I-Adarsh-I <adarshsingh9479020444@gmail.com>
2024-03-04 17:52:37 +0530

    Frontend file configured

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h4>
+        Welcome to 
+        <em> Note Down</em>
+      </h4>
+      <p>
+        Know about author -{" "}
+        <a href="https://adarshsingh.vercel.app" target="_blank" rel="noreferrer">Adarsh Singh</a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
