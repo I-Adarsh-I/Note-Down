e0ea3ef86c5d03e739e4607fb9a774fbed227407
I-Adarsh-I <adarshsingh9479020444@gmail.com>
2024-04-02 17:24:49 +0530

    Bugs release

---
+++ client/src/pages/profile/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavbarTop from "../../components/navbar/Navbar";
+
+const Profile = () => {
+
+  return (
+    <div className="mt-20">
+      <NavbarTop />
+    </div>
+  );
+};
+
+export default Profile;
