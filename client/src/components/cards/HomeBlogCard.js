b1c0ab5240e999d201f54122c69f28e26ff39268
I-Adarsh-I <adarshsingh9479020444@gmail.com>
2024-05-07 23:52:19 +0530

    Home page blog card - Ready

---
+++ client/src/components/cards/HomeBlogCard.js
@@ -0,0 +1,103 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Avatar,
+} from "@material-tailwind/react";
+import Tag from "../tags/Tag";
+
+const HomeBlogCard = () => {
+  return (
+    <div>
+      <Card className="w-full lg:max-w-[24rem] overflow-hidden my-4">
+        <CardHeader
+          floated={false}
+          shadow={false}
+          color="transparent"
+          className="m-0 rounded-none"
+        >
+            <div className="flex gap-2 p-4">
+            <Avatar
+              size="sm"
+              variant="circular"
+              alt="natali craig"
+              src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1061&q=80"
+              className="border-2 border-white hover:z-10"
+            />
+            <div className="flex flex-col justify-start">
+              <Typography variant="small" color="black">
+                Full Name
+              </Typography>
+              <Typography variant="small" color="gray" className="text-xs">
+                Apr, 20
+              </Typography>
+            </div>
+          </div>
+          <img
+            src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+            alt="ui/ux review check"
+            className="pb-4"
+          />
+        </CardHeader>
+        <CardBody className="pt-0 pb-4 px-4">
+          {/* <div className="flex gap-2 pb-2">
+            <Avatar
+              size="sm"
+              variant="circular"
+              alt="natali craig"
+              src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1061&q=80"
+              className="border-2 border-white hover:z-10"
+            />
+            <div className="flex flex-col justify-start">
+              <Typography variant="small" color="black">
+                Full Name
+              </Typography>
+              <Typography variant="small" color="gray" className="text-xs">
+                Apr, 20
+              </Typography>
+            </div>
+          </div> */}
+          <Typography variant="h4" color="blue-gray">
+            UI/UX Review Check
+          </Typography>
+          <Typography
+            color="gray"
+            className="mt-1 font-sm font-normal leading-tight"
+          >
+            Because it&apos;s about motivating the doers. Because I&apos;m here
+            to follow my dreams and inspire others.
+          </Typography>
+          <div className="w-full flex gap-2 flex-wrap pt-2">
+            <Tag fontSize={"text-xs"} tagContent={"Sample"} />
+            <Tag fontSize={"text-xs"} tagContent={"Sample"} />
+            <Tag fontSize={"text-xs"} tagContent={"Sample"} />
+          </div>
+        </CardBody>
+        <CardFooter className="flex items-center justify-between pb-4 pt-0">
+          <div className="flex items-center gap-2">
+            <div className="applauds">
+              <p className="text-sm text-center">
+                <i className="fa-solid fa-hands-clapping fa-lg pe-1 text-gray-600 cursor-pointer"></i>
+                11
+              </p>
+            </div>
+            <div className="comments">
+              <p className="text-sm">
+                <i className="fa-regular fa-comment fa-lg pe-1 text-gray-600 cursor-pointer"></i>
+                12
+              </p>
+            </div>
+          </div>
+          <Typography className="font-normal">
+            <span className="px-1">&#9679;</span>4 min
+          </Typography>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default HomeBlogCard;
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,14 +1,40 @@
 import React from "react";
 import NavbarTop from "../../components/navbar/Navbar";
+import HomeBlogCard from "../../components/cards/HomeBlogCard";
 
 const Home = () => {
   return (
     <>
       <NavbarTop />
-      <div className="home-page-hero w-full flex justify-center bg-gradient-to-b from-blue-50 via-purple-50 to-transparent">
-        <div className="container w-full flex flex-col items-center gap-1 lg:gap-3 py-24 px-4 lg:px-0">
-          <h5 className='font-semibold text-2xl lg:text-4xl md:text-3xl'>Explore All Blogs</h5>
-          <p className='text-gray-800 text-wrap text-center text-sm lg:text-base md:text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, sunt.</p>
+      <div className="home-page-hero w-full flex flex-col items-center">
+        <div className="w-full flex flex-col items-center gap-1 lg:gap-3 pt-24 mb-8 px-2 lg:px-0 bg-gradient-to-b from-blue-50 via-purple-50 to-transparent">
+          <h5 className="font-semibold text-2xl lg:text-4xl md:text-3xl">
+            Explore All Blogs
+          </h5>
+          <p className="text-gray-800 text-wrap text-center text-sm lg:text-base md:text-base">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, sunt.
+          </p>
+        </div>
+        <div className="container flex gap-6">
+          <div className="r-sec w-full lg:px-0 px-2 lg:px-0">
+            <div className="head py-2">
+              <p className="text-lg lg:text-xl md:text-xl font-medium w-max text-gray-900">
+                Recent
+              </p>
+              <div className="flex justify-center">
+                <div className="home-blog-cards grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-x-8 lg:gap-x-20 lg:gap-y-6">
+                  <HomeBlogCard />
+                  <HomeBlogCard />
+                  <HomeBlogCard />
+                  <HomeBlogCard />
+                </div>
+              </div>
+            </div>
+            <div className="featured-blogs"></div>
+          </div>
+          <div className="l-sec hidden">
+            <p>Left</p>
+          </div>
         </div>
       </div>
     </>
