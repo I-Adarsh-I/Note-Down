dd6c3f483e64e1ca6c9b201d52ba42fc8ee5822e
I-Adarsh-I <adarshsingh9479020444@gmail.com>
2024-05-14 22:06:15 +0530

    Bugs fixed

--- client/src/pages/create_blog/Write.js
+++ client/src/pages/create_blog/Write.js
@@ -1,12 +1,42 @@
-import React from 'react'
-import NavbarTop from '../../components/navbar/Navbar'
+import React, { useState, useRef } from "react";
+import NavbarTop from "../../components/navbar/Navbar";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { Button } from "@material-tailwind/react";
 
 const Write = () => {
+  const [editorHtml, setEditorHtml] = useState("");
+  const quillRef = useRef(null);
+
+  const toolbarOptions = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ["bold", "italic", "underline", "strike"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["link", "image"],
+    ["clean"],
+  ];
+
   return (
     <>
       <NavbarTop />
+      <div className="container mt-56">
+        <div className="w-screen flex flex-col items-center">
+          <ReactQuill
+            ref={quillRef}
+            theme="snow"
+            value={editorHtml}
+            onChange={setEditorHtml}
+            modules={{
+              toolbar: toolbarOptions,
+            }}
+          />
+          <Button onClick={() => console.log(editorHtml)} color="black">
+            Save
+          </Button>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Write
+export default Write;
